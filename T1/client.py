c11791baa96ba90d67f853ad0072728dd67bd39b
Pedro Salviano <pedrosalviano3@gmail.com>
2024-07-18 15:46:08 -0300

    feat: add framerate display

--- T1/client.py
+++ T1/client.py
@@ -6,8 +6,7 @@ import cv2
 import pyaudio
 import threading
 import numpy as np
-import struct
-import math
+import time
 import zlib
 
 SALT_SIZE = 16
@@ -81,6 +80,12 @@ def receive_video():
     global runningFlag
     global room_id
 
+    # used to record the time when we processed last frame 
+    prev_frame_time = 0
+    
+    # used to record the time at which we processed current frame 
+    new_frame_time = 0
+
     while runningFlag:
         try:
             topic, compressed_frame = VIDEO_SUB.recv_multipart()
@@ -105,6 +110,29 @@ def receive_video():
                     windowName = f"{participant}'s video"
                     video_windows[sender_id] = windowName
                     cv2.namedWindow(windowName)
+
+                # font which we will be using to display FPS 
+                font = cv2.FONT_HERSHEY_SIMPLEX 
+                # time when we finish processing for this frame 
+                new_frame_time = time.time() 
+            
+                # Calculating the fps 
+            
+                # fps will be number of frame processed in given time frame 
+                # since their will be most of time error of 0.001 second 
+                # we will be subtracting it to get more accurate result 
+                fps = 1/(new_frame_time-prev_frame_time) 
+                prev_frame_time = new_frame_time 
+            
+                # converting the fps into integer 
+                fps = int(fps) 
+            
+                # converting the fps to string so that we can display it on frame 
+                # by using putText function 
+                fps = str(fps) 
+            
+                # putting the FPS count on the frame 
+                cv2.putText(frame, fps, (7, 70), font, 1, (100, 255, 0), 2, cv2.LINE_AA) 
                 
                 cv2.imshow(video_windows[sender_id], frame)
 
